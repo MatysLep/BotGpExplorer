431139f1c91ba689cc77fb53de6fcc0a2c2cd1d0
matys <matys.lepretre@icloud.com>
2025-06-07 18:00:25 +0200

    ✓ Récupération des jours + ajout dans le panier quand c'est dimanche

--- bot.py
+++ bot.py
@@ -1,57 +1,90 @@
 import re
-from playwright.sync_api import Playwright, sync_playwright, expect
-
+import time
+import asyncio
+from playwright.async_api import async_playwright
 
 def convertir_en_secondes(texte: str) -> int:
     nombres = [int(num) for num in re.findall(r'\d+', texte)]
 
-    # minutes et secondes
     minutes = nombres[0] if len(nombres) > 0 else 0
     secondes = nombres[1] if len(nombres) > 1 else 0
 
     return (minutes * 60 + secondes) * 1000
 
+async def run(playwright) -> bool:
+    browser = await playwright.chromium.launch(headless=False)
+    context = await browser.new_context()
+    page = await context.new_page()
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+    await page.goto("https://gp-explorer.fr/billetterie/revente/")
+    html_content = await page.content()
+    await page.goto("https://widget.weezevent.com/ticket/gp-explorer-bourse-aux-billets?locale=fr-FR")
+    await page.wait_for_timeout(5000)
 
-    page.goto("https://gp-explorer.fr/billetterie/revente/")
-    html_content = page.content()
-    page.goto("https://widget.weezevent.com/ticket/gp-explorer-bourse-aux-billets?locale=fr-FR")
-    page.wait_for_timeout(5000)
+    if await page.query_selector("#wz-button-continue") is not None:
+        await (await page.query_selector("#wz-button-continue")).click()
 
-    if page.query_selector("#wz-button-continue") is not None:
-        page.query_selector("#wz-button-continue").click()
-
-    page.wait_for_selector(".queue-rank-number")
-
-    while True :
-        num_attente = page.query_selector(".queue-rank-number")
-        if num_attente is None :
+    while True:
+        num_attente = await page.query_selector(".queue-rank-number")
+        if num_attente is None:
             print("🎉élément a disparu🎉")
             break
         else:
-            print(f"⏰plus que {num_attente.inner_text()} places ⏰")
-            page.wait_for_selector(".queue-waiting-time-count-down")
-            time_wait = convertir_en_secondes(page.query_selector(".queue-waiting-time-count-down").inner_text())
-            print(f"temps à attendre : {time_wait} ")
-            page.wait_for_timeout(time_wait)
+            text = await num_attente.inner_text()
+            print(f"⏰plus que {text} places ⏰")
+            await page.wait_for_selector(".queue-waiting-time-count-down")
+            await page.wait_for_timeout(2000)
+            time_element = await page.query_selector(".queue-waiting-time-count-down")
+            time_text = await time_element.inner_text()
+            time_wait = convertir_en_secondes(time_text)
+            print(f"temps à attendre : {time_wait}")
+            await page.wait_for_timeout(time_wait)
 
-    message_description = page.query_selector(".wz-message-description")
-    if message_description is not None and message_description.inner_text() == "Aucun billet disponible pour le moment, veuillez essayer plus tard." :
+    message_description = await page.query_selector(".wz-message-description")
+    if message_description is not None and await message_description.inner_text() == "Aucun billet disponible pour le moment, veuillez essayer plus tard.":
         print("plus de billet disponible pour le moment...😢")
-    else :
+        await context.close()
+        await browser.close()
+        return True
+    else:
         print(f"Des billets sont en lignes")
-        # AJOUT DE LA SUITE DU CODE
-            ## REGARDER SI LES PLACES SONT CELLES DU DIMANCHE
-                ### APPUYER 4x SUR LE BOUTONS AJOUTER DES PLACES
-                    #### CLIQUER SUR LE BOUTON "CONTINUER" POUR LE PAIEMENT
-                        ##### AUTO REMPLIR LES INFORMATIONS DE MA CB
-    page.wait_for_timeout(99999999)
 
+        available_date = await page.query_selector_all(".neo-rate-group-title")
 
+        # Si des dates sont disponibles
+        if available_date is not None :
 
-playwright = sync_playwright().start()
-run(playwright)
+            # boucle sur chaque date
+            for date in available_date:
+                text = await date.inner_text()
+                print(f"billet(s) disponible(s) pour {text}")
+                # Si la date correspond à DIMANCHE
+                if text.strip() == "DIMANCHE":
+                    print("PLACE DISPONIBLE LE DIMANCHE !!!")
+                    # Appui 4 fois sur le bouton "+" pour avoir 4 places
+                    counter = await page.query_selector('button[aria-label^="Ajouter 1 billet"]')
+                    if counter:
+                        for _ in range(4):
+                            await counter.click()
+                            await page.wait_for_timeout(500)  # Petite pause pour être sûr que le compteur se met à jour
+                    else:
+                        print("Bouton pour ajouter des billets non trouvé.")
+                    # Attend le bouton Suite pour cliquer dessus
+                    await page.wait_for_selector(".wz-neo-button-2.wz-cta-next")
+                    await page.click(".wz-neo-button-2.wz-cta-next")
+                    await page.wait_for_timeout(99999999)
+                    return False
+        return True
+
+async def main():
+    restart = True
+    while restart:
+        try :
+            async with async_playwright() as playwright:
+                restart = await run(playwright)
+        except Exception as e:
+            print(e)
+        await asyncio.sleep(5)
+
+if __name__ == "__main__":
+    asyncio.run(main())
